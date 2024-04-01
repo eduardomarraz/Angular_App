c9e6a790975fb119966f6bb76976cfc2b6ecc961
Eduardo <eduardomr_@hotmail.com>
2024-04-01 21:21:28 +0200

    Finalizado

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -9,11 +9,34 @@ export class Person {
 
     constructor( 
         public name: string,
-        private address: string
+        private address?: string
         ) {}
 }
 
-const ironman = new Person('Ironman', 'New York');
+// export class Hero extends Person {  
+
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string,
+//     ) {
+//         super( realName, 'New York');
+//     }
+// }
+export class Hero {  
+
+    // public person: Person;
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person,
+    ) {
+        // this.person = new Person(realName);
+    }
+}
+const tony = new Person('Tony', 'New York');
+const ironman = new Hero('Ironman', 45, 'Tony', tony);
 
 console.log (ironman);
 
